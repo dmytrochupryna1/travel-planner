0bcd11cfb62932a9fb83019fb26fb067b128c4c5
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-30 14:16:55 +0100

    added data.js to store component data

--- src/data.js
+++ src/data.js
@@ -1,5 +1,17 @@
 export default [
     {
-        
+       img: "japan.png",
+        country: "Japan",
+        link: "https://maps.app.goo.gl/URBjqyWGfkabvpby5",
+        title: "Mount Fuji",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    }, {
+        img: "australia.png",
+        country: "Australia",
+        link: "https://maps.app.goo.gl/URBjqyWGfkabvpby5",
+        title: "Sydney Opera House",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre at Sydney Harbour in Sydney, New South Wales, Australia. It is one of the 20th century's most famous and distinctive buildings."
     }
 ]
