1c20d211cfaf86e67171b5e6d500226fd6994a69
Dimitri Chupryna <dmytro.chupryna.1@gmail.com>
2024-05-30 14:15:10 +0100

    rendering content through props

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ function App() {
 
   return (
     <>
-    <Destination />
-    <Destination />
+    <Destination
+      img="japan.png"
+      country="Japan"
+      link="https://maps.app.goo.gl/URBjqyWGfkabvpby5" 
+      title="Mount Fuji"
+      dates="12 Jan, 2021 - 24 Jan, 2021"
+      description="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    />
+    <Destination
+      img="australia.png"
+      country="Australia" 
+    />
     </>
   )
 }
--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,7 +1,18 @@
 export default function Destination(props) {
     return (
         <div class="destination">
-        <h1>Destination</h1>
+            <img src={props.img}/>
+            <div class="destination--description">
+                <div class="destination--description--first-line">
+                    <img src="pin-point.png"/>
+                    <h1>{props.country}</h1>
+                    <a href={props.link} target="_blank">View on Google Maps</a>
+                </div>
+                <h1>{props.title}</h1>
+                <h3>{props.dates}</h3>
+                <p>{props.description}</p>
+            </div>
+            
         </div>
     )
 }
